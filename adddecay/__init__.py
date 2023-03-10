d6f01fecb611f15f44989ef9ac29140fcb29b791
Jonathan Schaffner <jonathan.schaffner@students.fhnw.ch>
2023-03-10 16:15:52 +0100

    summed up decays

--- adddecay/__init__.py
+++ adddecay/__init__.py
@@ -12,17 +12,24 @@ decayed_images = {}
 images_as_Array = {}
 images_decayed = {}
 
-def decay(seq, imgArray, count):    
-    
-    decayed_img = imgArray // 5
-    
-    return imgArray
+images_processed = []
 
+def decay(idx, imgArray):    
+    print(idx)
+
+    sum_decayed = imgArray * 0
+
+    for image_name in images_processed:
+        # TODO: replace with actual decay function
+        sum_decayed += imgArray // (2 * idx)
+        print(image_name)
+    
+    return sum_decayed
 
 # TODO: pass as command line arguments
 fileprefix = 'ct5s_0000'
 ct_dir = 'C:\\Users\\Jonathan Schaffner\\FHNW_Projct\MuhRec\\SampleData\\04_ct5s375'
-output_base_dir = 'C:\\Users\\Jonathan Schaffner\\FHNW_Projct\MuhRec\GeneratedData'
+output_base_dir = 'C:\Users\Jonathan Schaffner\FHNW_Projct\MuhRec\GeneratedData'
 image_interval_ms = 50
 
 timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
@@ -41,13 +48,11 @@ for idx, file in enumerate(files) :
     # images as raw array
     images_as_Array[file] = numpy.array(images[file])
 
-    print(images_as_Array[file])
-    images_decayed[file] = decay(idx, images_as_Array[file], files.count)
-    print(images_as_Array[file])
+    images_decayed[file] = decay(idx, images_as_Array[file])
 
-    img = Image.fromarray(images_as_Array[file])
+    img = Image.fromarray(images_decayed[file])
 
+    images_processed.append(file)
     img.save(os.path.join(output_dir, file),format='TIFF')
-
 
 print("Finished!")
