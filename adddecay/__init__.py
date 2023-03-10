621390b19f86fb5b399670806438ba75428b0b69
Jonathan Schaffner <jonathan.schaffner@students.fhnw.ch>
2023-03-10 15:43:53 +0100

    Added basic image copy function

--- adddecay/__init__.py
+++ adddecay/__init__.py
@@ -8,11 +8,22 @@ from PIL import Image
 from pathlib import Path
 from datetime import datetime
 
+decayed_images = {}
+images_as_Array = {}
+images_decayed = {}
+
+def decay(seq, imgArray, count):    
+    
+    decayed_img = imgArray // 5
+    
+    return imgArray
+
 
 # TODO: pass as command line arguments
 fileprefix = 'ct5s_0000'
 ct_dir = 'C:\\Users\\Jonathan Schaffner\\FHNW_Projct\MuhRec\\SampleData\\04_ct5s375'
 output_base_dir = 'C:\\Users\\Jonathan Schaffner\\FHNW_Projct\MuhRec\GeneratedData'
+image_interval_ms = 50
 
 timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
 output_dir = Path(os.path.join(output_base_dir, 'AddRecursiveDecay', timestamp))
@@ -21,13 +32,22 @@ output_dir.mkdir(parents=True)
 files = os.listdir(ct_dir)
 files = list(filter(lambda x: x.startswith(fileprefix), files))
 images = {}
-imagesAsArray = {}
 
-for file in files :
+
+for idx, file in enumerate(files) :
     images[file] = Image.open(os.path.join(ct_dir, file))
+    decayed_images[file] = {}
 
     # images as raw array
-    imagesAsArray[file] = numpy.array(images[file])
+    images_as_Array[file] = numpy.array(images[file])
 
-print(images)
-print(imagesAsArray)
+    print(images_as_Array[file])
+    images_decayed[file] = decay(idx, images_as_Array[file], files.count)
+    print(images_as_Array[file])
+
+    img = Image.fromarray(images_as_Array[file])
+
+    img.save(os.path.join(output_dir, file),format='TIFF')
+
+
+print("Finished!")
