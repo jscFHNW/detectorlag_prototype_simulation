dd695ba757f1f58fbf2847b10bc1d74f74ecc51b
Jonathan Schaffner <jonathan.schaffner@students.fhnw.ch>
2023-03-24 14:42:38 +0100

    Added overflow prevention

--- adddecay/__init__.py
+++ adddecay/__init__.py
@@ -29,52 +29,60 @@ output_decayed = Path(os.path.join(output_dir, 'Decay'))
 output_dir.mkdir(parents=True)
 output_decayed.mkdir(parents=True)
 
+# file name of the images
+ct_files = []
+ob_files = []
+dc_files = []
+
 # images as PIL objects
 ct_imgs = {}
 ob_imgs = {}
 dc_imgs = {}
 
-# images as PIL objects
+# images as numpy arrays
 ct_imgs_arr = {}
 ob_imgs_arr = {}
 dc_imgs_arr = {}
+ct_imgs_arr_no_dc = {}
 
 # average of all the DC samples as a numpy array
-dc_avr = []
+dc_avr = None
 
-imgs_processed = []
+# numpy array Template
+array_temp = None
+
+# file names of alread processed images
+processed_files = []
 
 def main():    
 
     load_images()  
 
-    dc_avr = get_dc_average();  
+    get_dc_average();  
 
     # iterate through projections
-    for idx, img in enumerate(ct_imgs) :
+    for idx, file_name in enumerate(ct_files) :
 
-        # open projection image
-        #img = Image.open(os.path.join(ct_dir, file_name))
+        img = ct_imgs[file_name]
 
         # get tiffinfo from current image
         info = img.tag_v2
 
-        # images as raw array
-        #images_as_Array[file_name] = numpy.array(raw_img)
-
         # all previously decayed images summed up
-        decayed_array = decay()
-        img_decayed = Image.fromarray(decayed_array)
+        decay_img_arr = decay()
+        decay_img = Image.fromarray(decay_img_arr)
 
-        # add decay to current image
-        img_added = Image.fromarray(images_as_Array[file_name] + decayed_array)
+        # add decay to current image and prevent overflow
+        merged_img_arr = numpy.empty_like(decay_img_arr, dtype='uint32')
+        merged_img_arr = ct_imgs_arr[file_name].astype('uint32') + decay_img_arr.astype('uint32')       
+        merged_img = Image.fromarray(numpy.clip(merged_img_arr, 0, 65535).astype('uint16'))
 
         # save images with tiffinfo from original image to preserve metadata
-        img_decayed.save(os.path.join(output_decayed, "decay_" + str(idx).zfill(4) + ".tif"), format='TIFF', tiffinfo=info)
-        img_added.save(os.path.join(output_dir, "added_" + str(idx).zfill(4) + ".tif"), format='TIFF', tiffinfo=info)
+        decay_img.save(os.path.join(output_decayed, "decay_" + str(idx).zfill(4) + ".tif"), format='TIFF', tiffinfo=info)
+        merged_img.save(os.path.join(output_dir, "added_" + str(idx).zfill(4) + ".tif"), format='TIFF', tiffinfo=info)
 
         # add image to the processed list
-        images_processed.append(file_name)
+        processed_files.append(file_name)
 
         print(file_name + " processed!")
         
@@ -90,7 +98,8 @@ def load_images():
     all_files = os.listdir(ct_dir)
 
     # get OB images
-    ob_files = list(filter(lambda x: x.startswith(prefix_ob) == False, all_files))
+    global ob_files
+    ob_files = list(filter(lambda x: x.startswith(prefix_ob), all_files))
 
     # copy OB images
     for file in ob_files :
@@ -99,10 +108,12 @@ def load_images():
         shutil.copyfile(source, target)
 
         # load images
-        ob_imgs[file] = Image.open(os.path.join(output_dir, file))
+        ob_imgs[file] = Image.open(target)
+        ob_imgs_arr[file] = numpy.array(ob_imgs[file])
 
     # get DC images
-    dc_files = list(filter(lambda x: x.startswith(prefix_dc) == False, all_files))
+    global dc_files
+    dc_files += list(filter(lambda x: x.startswith(prefix_dc), all_files))
 
     # copy DC images
     for file in dc_files :
@@ -111,44 +122,56 @@ def load_images():
         shutil.copyfile(source, target)
 
         # load images
-        dc_imgs[file] = Image.open(os.path.join(output_dir, file))
+        dc_imgs[file] = Image.open(target)
         dc_imgs_arr[file] = numpy.array(dc_imgs[file])
 
     # get projection images
-    ct_files = list(filter(lambda x: x.startswith(prefix_ct), all_files))
+    global ct_files
+    ct_files += list(filter(lambda x: x.startswith(prefix_ct), all_files))
 
     for file in ct_files :
         source = os.path.join(ct_dir, file)
 
         # load images
-        ct_imgs[file] = Image.open(os.path.join(source, file))
-        ct_imgs_arr[file] = numpy.array(dc_imgs[file])
+        ct_imgs[file] = Image.open(source)
+        ct_imgs_arr[file] = numpy.array(ct_imgs[file])
+
+        # set numpy array template
+        global array_temp
+        array_temp = numpy.empty_like(ct_imgs_arr[file], dtype='uint16')
 
 
+## calculates the average DC image as a numpy array from the dc samples
 def get_dc_average():
 
-    sum = []
+    sum = array_temp
+    sum *= 0
 
-    for img_arr in dc_imgs_arr :
-        sum += img_arr
+    for name, arr in dc_imgs_arr.items() :
+        sum += arr
 
-    return sum / len(dc_imgs_arr)
+    global dc_avr
+    dc_avr =  sum // len(dc_imgs_arr)   
+
+    for name, arr in ct_imgs_arr.items() : 
+        ct_imgs_arr_no_dc[name] = arr - dc_avr
 
 
-def decay(imgArray):
+## sums up and returns the renmants of the decayed afterimages in a numpy array
+def decay():
 
-    sum_decayed = imgArray * 0
+    sum_decayed = array_temp
+    sum_decayed *= 0
 
-    img_count = len(imgs_processed)
+    img_count = len(processed_files)
 
-    for idx, image_name in reversed(list(enumerate(imgs_processed))):
+    for idx, image_name in reversed(list(enumerate(processed_files))):
         # TODO: replace with actual decay function 
-        # using an increasing index instead of a function 
-        # subtract dc_avr from image
-        sum_decayed += (ct_imgs_arr[image_name] - dc_avr) // (5 * (img_count - idx)) 
-        
-    return sum_decayed
+        # using an increasing index instead of a time based function 
+        # sum_decayed += (ct_imgs_arr[image_name]) // (20 * (img_count - idx)) 
+        sum_decayed += ct_imgs_arr_no_dc[image_name] // (3 * (img_count - idx)) 
 
+    return sum_decayed
 
 if __name__ == "__main__":
     main()
